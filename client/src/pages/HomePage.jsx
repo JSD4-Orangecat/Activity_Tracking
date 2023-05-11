7623829527cdf3b0b810edad0d49aa4d7380c7d0
Peeranat Phengsalaeh <seed.peeranat@gmail.com>
2023-05-11 14:14:48 +0700

    initial commit

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+// import NavBar from "../components/NavBar";
+// import Footer from "../components/Footer";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>home</div>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default HomePage;
