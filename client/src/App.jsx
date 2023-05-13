fea0ba9491a284b64bede42e43d157feb2c6f23f
Peeranat Phengsalaeh <seed.peeranat@gmail.com>
2023-05-13 18:55:20 +0700

    feat: add authentication feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,12 @@
-import "./assets/styles/App.css";
-import HomePage from "./pages/HomePage";
-import CreateCard from "./pages/CreateCard";
-import Dashboard from "./pages/DashBoard";
-import EditCard from "./pages/EditCard";
-import Login from "./pages/Login";
-import { Routes, Route } from "react-router-dom";
-
-import FormRegister from "./pages/RegisterPage";
-
-import ReadCard from "./pages/ReadCard";
-
+import { useAuth } from "./contexts/authentication";
+import AuthenticatedApp from "./pages/AuthenticatedApp";
+import UnauthenticatedApp from "./pages/UnauthenticatedApp";
 
 function App() {
+  const auth = useAuth();
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-
-        <Route path="/register" element={<FormRegister />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/createcard" element={<CreateCard />} />
-        <Route path="/editcard" element={<EditCard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/readcard" element={<ReadCard />} />
-
-      </Routes>
+      {auth.isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
---
+++ client/src/pages/AuthenticatedApp.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import CreateCard from "./CreateCard";
+import Dashboard from "./DashBoard";
+import EditCard from "./EditCard";
+import ReadCard from "./ReadCard";
+
+function AuthenticatedApp() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/createcard" element={<CreateCard />} />
+        <Route path="/editcard" element={<EditCard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/readcard" element={<ReadCard />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </div>
+  );
+}
+
+export default AuthenticatedApp;
