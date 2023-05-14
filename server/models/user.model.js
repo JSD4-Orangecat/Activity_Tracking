d8a1b734f13c6bb996b6815316a3b7493b15e415
Joakim <yukimever@gmail.com>
2023-05-14 21:38:47 +0700

    feat: add model user

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,39 +1,58 @@
 import mongoose from "mongoose";
-const { Schema } = mongoose;
 
-const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
+const { Schema } = mongoose
+const userSchema = new Schema({
+    "email": {
+        "type": String,
+        "required": true,
+        "unique": true
     },
-    password: {
-      type: String,
-      required: true,
+    "password": {
+        "type": String,
+        "required": true
     },
-    firstName: {
-      type: String,
-      required: true,
+    "firstName": {
+        "type": String,
+        "required": true
     },
-    lastName: {
-      type: String,
-      required: true,
+    "lastName": {
+        "type": String,
+        "required": true
     },
-    birthDate: {
-      type: String,
-      required: true,
+    "birthDate": {
+        "type": String,
+        "required": true
     },
-    gender: {
-      type: String,
-      required: true,
-      enum: ["male", "female"],
+    "gender": {
+        "type":String,
+        "required": true,
+        "enum": ["male","female"]
     },
-    picture: {
-      type: String,
+    "picture": {
+        "type": String,
+        "required": false
     },
-  },
-  { collection: "user" }
-);
+    "weight": {
+        "type": Number,
+        "required": true
+    },
+    "height": {
+        "type": Number,
+        "required": true
+    },
+    "coverImage": {
+        "type": String,
+        "required": false
+    },
+    "quote": {
+        "type": String,
+        "required": false
+    },
+    "emoji": {
+        "type": String,
+        "required": false
+    }
+}, {collection: 'user'})
 
-export default mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
+
