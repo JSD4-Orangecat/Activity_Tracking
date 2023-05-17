a5e185ffab162c7fdbbab1b8b5e2dbb2c709c86d
Haluethaichanok-J <haluethaichanok@gmail.com>
2023-05-17 10:15:24 +0700

    fix-praew-pregisterpage-rebase

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../components/Layout";
+
+import "../assets/styles/Profile.css";
+
+function Profile() {
+  return (
+    <Layout>
+      <div className="allProfile">
+        <section className="top-sec">
+          <div className="wrapPhoto">
+            <img />
+          </div>
+          <div className="informProfile"></div>
+        </section>
+        <section className="bot-sec"></section>
+        profile
+      </div>
+    </Layout>
+  );
+}
+
+export default Profile;
