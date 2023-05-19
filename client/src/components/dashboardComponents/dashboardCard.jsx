7738ef68e29120f90890023c47e2e358993b07a4
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-19 10:21:28 +0700

    Remove unnecessary parts

--- client/src/components/dashboardComponents/dashboardCard.jsx
+++ client/src/components/dashboardComponents/dashboardCard.jsx
@@ -4,19 +4,13 @@ import Card from "../readCardComponents/readCardCard";
 import '../../assets/styles/dashboardCSS/dashboardCard.css';
 
 export default function DashboardCards() {
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-    const [limit, setLimit] = useState(3);
     const [getActivity, setGetActivity] = useState([]);
 
     const fetchActivity = async () => {
         try {
-            const response = await axios.get("http://127.0.0.1:4000/activities", {
-                params: { page, limit },
-            });
-            const { data, totalDocs } = response.data;
+            const response = await axios.get("http://127.0.0.1:4000/activities");
+            const { data } = response.data;
             setGetActivity(data);
-            setTotalPages(Math.ceil(totalDocs / limit));
         } catch (err) {
             console.error(err);
         }
@@ -24,11 +18,13 @@ export default function DashboardCards() {
 
     useEffect(() => {
         fetchActivity();
-    }, [page, limit]);
+    }, []);
+
+    const renderActivity = getActivity.slice(0, 3);
 
     return (
         <div className="cards-container">
-            {getActivity.map((ele) => (
+            {renderActivity.map((ele) => (
                 <Card key={ele._id} data={ele} fetchActivity={fetchActivity} />
             ))}
         </div>
