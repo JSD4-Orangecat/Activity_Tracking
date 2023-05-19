f4f3d42e4f38af7fd303ded832ddb5322abf8308
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-19 10:04:45 +0700

    fetchActivity successful

--- client/src/components/dashboardComponents/dashboardCard.jsx
+++ client/src/components/dashboardComponents/dashboardCard.jsx
@@ -1,61 +1,51 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Card from "../readCardComponents/readCardCard";
 import '../../assets/styles/dashboardCSS/dashboardCard.css';
 
-import biking from '/biking.png';
-import running from '/running.png';
-import swimming from '/swimming.png';
-import cardio from '/cardio.png';
-
 export default function DashboardCards() {
+
+    const pageLimit = 4;
+
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [limit, setLimit] = useState(pageLimit);
+    const [getactivity, setGetActivity] = useState([]);
+
+
+
+    const fetchActivity = async () => {
+        try {
+            const response = await axios.get("http://127.0.0.1:4000/activities", {
+                params: {
+                    page,
+                    limit,
+                }
+            });
+            setGetActivity(response.data.data);
+            //Get total of document in database and calculate total pages.
+            const { totalDocs } = response.data
+            const totalPages = Math.ceil(totalDocs / limit)
+            setTotalPages(totalPages)
+
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+
+
+    useEffect(() => {
+        fetchActivity();
+    }, [page, limit]);
+
+
 
     return (
         <div className="cards-container">
-            {cardMockData.map((ele) =>
-                <Card key={ele.id} data={ele} />
-            )}
+            {getactivity.map((ele) => (
+                <Card key={ele._id} data={ele} fetchActivity={fetchActivity} />
+            ))}
         </div>
     );
 }
-
-const cardMockData = [
-    {
-        id: 1,
-        title: 'This is title',
-        caption: 'I wish I was a cat, no school, no work, no exercise, just meow meow meow meow meow',
-        duration: '1 h 30 m',
-        date: '2023-03-18',
-        task: '',
-        image: 'https://images.unsplash.com/photo-1501147830916-ce44a6359892?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        activity: { biking }
-    },
-    {
-        id: 2,
-        title: 'keep pushing yourself',
-        caption: ' meow meow meow meow meow',
-        duration: '2 h 10 m',
-        date: '2023-04-17',
-        task: '',
-        image: 'https://images.unsplash.com/photo-1621244246296-c6be56267cc3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1222&q=80',
-        activity: { running }
-    },
-    {
-        id: 3,
-        title: 'No time to sleep sleep sleep sleep sleep sleep sleep sleep sleeps',
-        caption: 'I eat, sleep, and run',
-        duration: ' 30 m',
-        date: '2023-03-16',
-        task: '',
-        image: 'https://plus.unsplash.com/premium_photo-1680981143179-8a6cd94d2901?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80',
-        activity: { swimming }
-    },
-    // {
-    //     id: 4,
-    //     title: 'no title to display',
-    //     caption: 'last test',
-    //     duration: '1 h 48 m',
-    //     date: '2023-03-07',
-    //     task: '',
-    //     image: 'https://images.unsplash.com/photo-1557330359-ffb0deed6163?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    //     activity: { cardio }
-    // }
-]
