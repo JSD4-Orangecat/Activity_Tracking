240a3bb0422bd760dec7678ea032e034aa65e3c2
Haluethaichanok-J <haluethaichanok@gmail.com>
2023-05-23 05:12:44 +0700

    feat-praew-profile-get-file-but-not-done

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -6,67 +6,33 @@ import { cloudinaryUploadProfile } from "../utils/upload.js";
 export const register = async (req, res) => {
   const height = Number(req.body.height);
   const weight = Number(req.body.weight);
-  if (req.file) {
-    try {
-      const uploadedImage = await cloudinaryUploadProfile(req.file);
-      // console.log(req.body)
-      const { email, password } = req.body;
+  try {
+    const uploadedImage = await cloudinaryUploadProfile(req.file);
+    // console.log(req.body)
+    const { email, password } = req.body;
 
-      // Check that email must be unique
-      const isUniqueEmail = await User.findOne({ email });
-      if (isUniqueEmail) {
-        return res
-          .status(400)
-          .json({ message: "This email already registered" });
-      }
-
-      // Hash password
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-
-      const user = new User({
-        ...req.body,
-        password: hashedPassword,
-        weight: weight,
-        height: height,
-        picture: uploadedImage,
-      });
-      console.log({ user });
-      await user.save();
-      res.status(201).send({ message: "Create user succesfully" });
-    } catch (err) {
-      res.status(500).send({ message: err.message });
+    // Check that email must be unique
+    const isUniqueEmail = await User.findOne({ email });
+    if (isUniqueEmail) {
+      return res.status(400).send({ message: "This email already registered" });
     }
-  } else {
-    try {
-      // console.log(req.body)
-      const { email, password } = req.body;
 
-      // Check that email must be unique
-      const isUniqueEmail = await User.findOne({ email });
-      if (isUniqueEmail) {
-        return res
-          .status(400)
-          .send({ message: "This email already registered" });
-      }
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-      // Hash password
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-
-      const user = new User({
-        ...req.body,
-        password: hashedPassword,
-        weight: weight,
-        height: height,
-        picture: "",
-      });
-      console.log({ user });
-      await user.save();
-      res.status(201).send({ message: "Create user succesfully" });
-    } catch (err) {
-      res.status(500).send({ message: err.message });
-    }
+    const user = new User({
+      ...req.body,
+      password: hashedPassword,
+      weight: weight,
+      height: height,
+      picture: uploadedImage,
+    });
+    console.log({ user });
+    await user.save();
+    res.status(201).send({ message: "Create user successfully" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
 };
 
@@ -101,12 +67,9 @@ export const login = async (req, res) => {
 
     return res.json({ message: "login successfully", token });
   } catch (err) {
-    return res
-      .status(400)
-      .json({
-        message: "Something went wrong!!",
-      })
-      .send(err.message);
+    return res.status(400).json({
+      message: err.message,
+    });
   }
 };
 // delete user account
