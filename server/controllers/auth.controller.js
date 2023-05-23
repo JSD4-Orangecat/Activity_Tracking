af3f2f7349535db7447f062ea1027a18040e8196
Haluethaichanok-J <haluethaichanok@gmail.com>
2023-05-23 11:04:25 +0700

    feat-praew-profile-feature-all-done

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -115,55 +115,99 @@ export const getUser = async (req, res, next) => {
 
 // Edit profile
 export const editProfile = async (req, res, next) => {
-  try {
-    // console.log(req.body)
-    const { email, password } = req.body;
+  if (req.file) {
+    try {
+      // console.log(req.body)
+      const { email, password } = req.body;
 
-    // assign req.user to variable userid
-    const userid = req.user.info._id;
-    // Find the user by ID
-    const account = await User.findById({ _id: userid });
+      // assign req.user to variable userid
+      const userid = req.user.info._id;
+      // Find the user by ID
+      const account = await User.findById({ _id: userid });
 
-    // Check if the user exists
-    if (!account) {
-      return res.status(404).send({ message: "User not found" });
-    }
-
-    // Check if the email is already registered by another user
-    if (email !== account.email) {
-      //find user email in database
-      const isUniqueEmail = await User.findOne({ email });
-      //if find email in database and found response
-      if (isUniqueEmail) {
-        return res
-          .status(400)
-          .send({ message: "This email is already registered" });
+      // Check if the user exists
+      if (!account) {
+        return res.status(404).send({ message: "User not found" });
       }
-    }
-    // Update the user's password if provided
-    if (password) {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      account.password = hashedPassword;
-    }
-    //upload picture profile
-    const uploadedImage = await cloudinaryUploadProfile(req.file);
-    // Update other profile fields
-    account.email = email || account.email;
-    account.firstName = req.body.firstName || account.firstName;
-    account.lastName = req.body.lastName || account.lastName;
-    account.birthDate = req.body.birthDate || account.birthDate;
-    account.gender = req.body.gender || account.gender;
-    account.picture = uploadedImage || account.picture;
-    account.weight = req.body.weight || account.weight;
-    account.height = req.body.height || account.height;
 
-    await account.save();
+      // Check if the email is already registered by another user
+      if (email !== account.email) {
+        //find user email in database
+        const isUniqueEmail = await User.findOne({ email });
+        //if find email in database and found response
+        if (isUniqueEmail) {
+          return res
+            .status(400)
+            .send({ message: "This email is already registered" });
+        }
+      }
 
-    res.status(200).send({ message: "Profile updated successfully" });
-  } catch (err) {
-    res
-      .status(500)
-      .send({ message: err.message + "Failed to update user account" });
+      //upload picture profile
+      const uploadedImage = await cloudinaryUploadProfile(req.file);
+      // Update other profile fields
+      account.password = req.body.password;
+      account.email = email || account.email;
+      account.firstName = req.body.firstName || account.firstName;
+      account.lastName = req.body.lastName || account.lastName;
+      account.birthDate = req.body.birthDate || account.birthDate;
+      account.gender = req.body.gender || account.gender;
+      account.picture = uploadedImage || account.picture;
+      account.weight = req.body.weight || account.weight;
+      account.height = req.body.height || account.height;
+
+      await account.save();
+
+      res.status(200).send({ message: "Profile updated successfully" });
+    } catch (err) {
+      res
+        .status(500)
+        .send({ message: err.message + "Failed to update user account" });
+    }
+  } else {
+    try {
+      // console.log(req.body)
+      const { email, password } = req.body;
+
+      // assign req.user to variable userid
+      const userid = req.user.info._id;
+      // Find the user by ID
+      const account = await User.findById({ _id: userid });
+
+      // Check if the user exists
+      if (!account) {
+        return res.status(404).send({ message: "User not found" });
+      }
+
+      // Check if the email is already registered by another user
+      if (email !== account.email) {
+        //find user email in database
+        const isUniqueEmail = await User.findOne({ email });
+        //if find email in database and found response
+        if (isUniqueEmail) {
+          return res
+            .status(400)
+            .send({ message: "This email is already registered" });
+        }
+      }
+
+      // Update other profile fields
+      account.password = req.body.password;
+      account.email = email || account.email;
+      account.firstName = req.body.firstName || account.firstName;
+      account.lastName = req.body.lastName || account.lastName;
+      account.birthDate = req.body.birthDate || account.birthDate;
+      account.gender = req.body.gender || account.gender;
+      account.picture = account.picture;
+      account.weight = req.body.weight || account.weight;
+      account.height = req.body.height || account.height;
+
+      await account.save();
+
+      res.status(200).send({ message: "Profile updated successfully" });
+    } catch (err) {
+      res
+        .status(500)
+        .send({ message: err.message + "Failed to update user account" });
+    }
   }
 };
