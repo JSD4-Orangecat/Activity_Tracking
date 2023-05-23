1925c1f6caa0aa3a63a53be3488b331f926787fa
Peeranat Phengsalaeh <seed.peeranat@gmail.com>
2023-05-23 14:30:59 +0700

    fix: add error message on registerpage and alertbox

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,6 +6,7 @@ import Layout from "../components/Layout";
 import axios from "axios";
 import Cat from "/hero/hero-img.png";
 import "../assets/styles/authenticateCSS/RegisterPage.css";
+import swal from "sweetalert";
 
 function FormRegister() {
   // useStates and variables
@@ -13,6 +14,8 @@ function FormRegister() {
   const [formErrors, setFormErrors] = useState({});
   const [srcImg, setSrcImg] = useState(null);
   const [isSubmit, setIsSubmit] = useState(false);
+  const [error, setError] = useState("");
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const [formValues, setFormValues] = useState({
     email: "",
@@ -45,10 +48,12 @@ function FormRegister() {
   // Function to handle save inputs
   const saveInput = async (e) => {
     e.preventDefault();
+    setIsSubmit(true);
     const error = validate(formValues);
     setFormErrors(error);
 
     if (Object.keys(error).length === 0) {
+      setIsProcessing(!isProcessing);
       const { confirmpassword, ...userData } = formValues;
       const formData = new FormData();
 
@@ -65,11 +70,12 @@ function FormRegister() {
           }
         );
 
-        setIsSubmit(true);
+        swal("Registered!", "Let's get started!", "success");
         navigate("/login");
-
       } catch (error) {
-        console.log(error);
+        setError(error.response.data.message);
+        setIsSubmit(false);
+        swal("Oops", "Something went wrong!", "error");
       }
     }
   };
@@ -78,7 +84,9 @@ function FormRegister() {
     window.scrollTo(0, 0);
   }, []);
 
-
+  useEffect(() => {
+    setIsProcessing(false);
+  }, [error, isSubmit]);
 
   return (
     <Layout>
@@ -91,6 +99,8 @@ function FormRegister() {
           formValues={formValues}
           formErrors={formErrors}
           saveInput={saveInput}
+          error={error}
+          isProcessing={isProcessing}
         />
         <div className="register-cats">
           <img src={Cat} alt="Muscular Orange Cat" className="right" />
