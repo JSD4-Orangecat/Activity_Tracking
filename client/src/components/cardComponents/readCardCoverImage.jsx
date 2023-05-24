d832b3810d205d39eca7084e0ef407e45648776e
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-24 09:20:15 +0700

    refactor: Clean CoverImage

--- client/src/components/cardComponents/readCardCoverImage.jsx
+++ client/src/components/cardComponents/readCardCoverImage.jsx
@@ -1,21 +1,16 @@
-/* eslint-disable react/prop-types */
+import React from 'react';
 import '../../assets/styles/cardCSS/readCardCoverImage.css';
 
-function CoverImage(props) {
-
-    const { cover, handleFileChange } = props;
-
-    return (
-        <main className="cover-container">
-            <input type="file" accept="image/*" id="imgs" className="cover-input-field" name='coverimage'
-                onChange={handleFileChange} ></input>
-            <img src={cover} className="cover-img-upload" name='coverImg' />
-            <div className='btn-changecover'>
-                <label className='click-here' htmlFor='imgs'>Change cover</label>
-            </div>
-
-        </main>
-    )
-}
+const CoverImage = ({ cover, handleFileChange }) => (
+    <main className="cover-container">
+        <input type="file" accept="image/*" id="imgs" className="cover-input-field" name="coverimage" onChange={handleFileChange} />
+        <img src={cover} className="cover-img-upload" name="coverImg" />
+        <div className="btn-changecover">
+            <label className="click-here" htmlFor="imgs">
+                Change Cover
+            </label>
+        </div>
+    </main>
+);
 
 export default CoverImage;
